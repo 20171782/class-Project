e1e2924af5ae8a2be4df522e1ec8e251ec75695a
Dalton Smith <dsmith27@my.ncu.edul.jm>
2019-12-05 16:59:43 -0500

    events page

--- src/components/events/events.vue
+++ src/components/events/events.vue
@@ -1,67 +1,18 @@
 <template>
     <div class="top " style="padding: 20px">
 
-        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="ratio: 7:3; animation: push;autoplay: true;autoplay-interval: 2000">
+        <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1" uk-slider="ratio: 7:3; animation: push;autoplay: false;autoplay-interval: 2000">
 
             <ul class="uk-slider-items uk-child-width-1-2 uk-child-width-1-3@s uk-child-width-1-4@m">
                 <li>
                     <div>
-                        <div class="uk-inline">
-                            <img src="https://image.shutterstock.com/image-photo/business-coach-rear-view-man-260nw-439214011.jpg" alt="">
-                            <div class="uk-overlay-primary uk-position-cover"></div>
-                            <div class="uk-overlay uk-position-bottom uk-light">
-                                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <div class="uk-inline" >
+                            <div uk-lightbox  class="uk-inline-clip uk-transition-toggle" tabindex="0" >
+                                <a class="uk-button uk-button-default" href="https://cdn.pixabay.com/photo/2019/10/15/13/40/winter-4551699__340.jpg" data-caption="Caption"><img
+                                        class="uk-transition-scale-up uk-transition-opaque"  src="https://cdn.pixabay.com/photo/2019/10/15/13/40/winter-4551699__340.jpg" alt=""></a>
+                            <div class="uk-position-bottom uk-card-body" uk-dark ><p class="uk-text-bold">Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p></div>
                             </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div>
-                        <div class="uk-inline">
-                            <img src="https://image.shutterstock.com/image-photo/events-concept-260nw-580432891.jpg" alt="">
-                            <div class="uk-overlay-primary uk-position-cover"></div>
-                            <div class="uk-overlay uk-position-bottom uk-light">
-                                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div>
-                        <div class="uk-inline">
-                            <img src="https://image.shutterstock.com/image-photo/audience-listens-lecturer-conference-hall-260nw-704922217.jpg" alt="">
-                            <div class="uk-overlay-primary uk-position-cover"></div>
-                            <div class="uk-overlay uk-position-bottom uk-light">
-                                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div>
-                        <div class="uk-inline">
-                            <img src="https://image.shutterstock.com/image-photo/truck-transport-container-on-road-260nw-197539418.jpg" alt="">
-                            <div class="uk-overlay-primary uk-position-cover"></div>
-                            <div class="uk-overlay uk-position-bottom uk-light">
-                                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div>
-                        <div class="uk-inline">
-                            <img src="https://cdn.pixabay.com/photo/2016/10/10/14/44/train-1728537__340.jpg" alt="">
-                            <div class="uk-overlay-primary uk-position-cover"></div>
-                            <div class="uk-overlay uk-position-bottom uk-light">
-                                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
-                        </div>
-                    </div>
-                </li> <li>
-                    <div>
-                        <div class="uk-inline">
-                            <img src="https://cdn.pixabay.com/photo/2015/10/12/14/55/airplane-983991__340.jpg" alt="">
-                            <div class="uk-overlay-primary uk-position-cover"></div>
-                            <div class="uk-overlay uk-position-bottom uk-light">
-                                <p>Default Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                            </div>
+
                         </div>
                     </div>
                 </li>
@@ -72,22 +23,6 @@
             <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" uk-slidenav-next uk-slider-item="next"></a>
 
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     </div>
 </template>
@@ -103,4 +38,16 @@ img{
     width: 450px;
     height: 300px;
 }
+    .top{
+       margin-top: 100px;
+       margin-bottom: 100px;
+    }
+
+    img{
+        height: 600px;
+    }
+    .uk-inline{
+        opacity: 0.7;
+    }
+    p{color: #db0f31}
 </style>
